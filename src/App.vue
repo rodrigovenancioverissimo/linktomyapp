d9090cbef7531d18406c6b473b84a1a816f0048e
Rodrigo Verissimo <rodrigo.verissimo@ignicaodigital.com.br>
2022-02-01 11:12:26 -0300

    adicionado redirect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Redirect/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Redirect from './pages/Redirect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Redirect
   }
 }
 </script>
